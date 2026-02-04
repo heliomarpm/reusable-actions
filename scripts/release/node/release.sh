ee367b4a4c645b73ec79a9a4a7e11db7d881cf49
Heliomar P. Marques <heliomarpm@gmail.com>
2026-02-03 21:35:05 -0300

    build(release): 👷 ajustar caminho do script de detecção de releaserc
    * Atualiza o caminho relativo para o script 'detect-releaserc-consumer.sh'

--- scripts/release/node/release.sh
+++ scripts/release/node/release.sh
@@ -33,7 +33,7 @@ has_file() {
 # ------------------------------------------------------------
 # Detect semantic-release config
 # ------------------------------------------------------------
-CUSTOM_CONFIG=$(../detect-releaserc-consumer.sh)
+CUSTOM_CONFIG=$(./scripts/release/detect-releaserc-consumer.sh)
 
 # ----------------------------------------------------------
 # Install dependencies ONLY if needed
